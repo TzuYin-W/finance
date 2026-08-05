f3b03a38a5f6b4f302bf6211411548085158eb05
TzuYin-W <wtzuyin@gmail.com>
2026-08-05 09:37:54 +0800

    Delete START_LOCAL_SERVER.bat

--- START_LOCAL_SERVER.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-start "" http://localhost:8000/index.html
-where py >nul 2>nul && py -m http.server 8000 && exit /b
-where python >nul 2>nul && python -m http.server 8000 && exit /b
-echo 找不到 Python。雲端登入需要 HTTPS 網址或 localhost，請安裝 Python 或部署到靜態網站。
-pause
