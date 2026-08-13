c261602d09cae273e10d6d161251ecd41672b3d4
TzuYin-W <wtzuyin@gmail.com>
2026-08-13 17:12:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='finance-tracker-20260813-design-board-v8';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./assets/themes/monchhichi/monchhichi-bg.png','./assets/themes/monchhichi/monchhichi-sidebar.png','./assets/themes/monchhichi/monchhichi-bg-landscape.png','./assets/themes/pokemon/pokemon-bg.png','./assets/themes/pokemon/pokemon-bg-landscape.png','./assets/themes/pokemon/app-icon-source.png','./assets/themes/pikmin/pikmin-bg.png','./assets/themes/pikmin/pikmin-bg-landscape.png'];
+const CACHE='finance-tracker-20260813-pikmin-pokemon-redesign-v9';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./assets/themes/monchhichi/monchhichi-bg.png','./assets/themes/monchhichi/monchhichi-sidebar.png','./assets/themes/monchhichi/monchhichi-bg-landscape.png','./assets/themes/pokemon/pokemon-bg.png','./assets/themes/pokemon/pokemon-bg-landscape.png','./assets/themes/pikmin/pikmin-bg.png','./assets/themes/pikmin/pikmin-bg-landscape.png','./assets/themes/pikmin/pikmin-sidebar.png','./assets/themes/pokemon/pokemon-sidebar.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
