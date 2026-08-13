6ad81f2ae1cb5e7ffce68bbc6141dbf49ea44b69
TzuYin-W <wtzuyin@gmail.com>
2026-08-13 18:47:14 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='finance-tracker-20260813-pikmin-pokemon-redesign-v9';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./assets/themes/monchhichi/monchhichi-bg.png','./assets/themes/monchhichi/monchhichi-sidebar.png','./assets/themes/monchhichi/monchhichi-bg-landscape.png','./assets/themes/pokemon/pokemon-bg.png','./assets/themes/pokemon/pokemon-bg-landscape.png','./assets/themes/pikmin/pikmin-bg.png','./assets/themes/pikmin/pikmin-bg-landscape.png','./assets/themes/pikmin/pikmin-sidebar.png','./assets/themes/pokemon/pokemon-sidebar.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
-});
