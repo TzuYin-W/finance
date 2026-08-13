72a7809f8fea8094c5ced2546104c3c0a9fafb1b
TzuYin-W <wtzuyin@gmail.com>
2026-08-13 13:19:35 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='finance-tracker-20260813-quota-fix-monchhichi-v4';
+const CACHE='finance-tracker-20260813-quota-fix-monchhichi-v5-glass';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./assets/themes/monchhichi/monchhichi-bg.png','./assets/themes/monchhichi/monchhichi-sidebar.png','./assets/themes/monchhichi/monchhichi-bg-landscape.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
