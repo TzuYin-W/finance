a27362140194df80c1929d64ce2cca24e34e8e50
TzuYin-W <wtzuyin@gmail.com>
2026-08-13 13:10:27 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='finance-tracker-20260813-quota-fix-monchhichi-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./assets/themes/monchhichi/monchhichi-bg.png','./assets/themes/monchhichi/monchhichi-sidebar.png'];
+const CACHE='finance-tracker-20260813-quota-fix-monchhichi-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/apple-touch-icon.png','./assets/themes/monchhichi/monchhichi-bg.png','./assets/themes/monchhichi/monchhichi-sidebar.png','./assets/themes/monchhichi/monchhichi-bg-landscape.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
