80cbf7ade92c5fd1e5a354420d246bed5789cd3a
TzuYin-W <wtzuyin@gmail.com>
2026-08-25 13:46:34 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='finance-tracker-20260825-v33-large-card-transparency';
+const CACHE='finance-tracker-20260825-v34-cash-account-edit-large-card-slider';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest',
   './icon-192-v23.png','./icon-512-v23.png','./apple-touch-icon-v23.png',
