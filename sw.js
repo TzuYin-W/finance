3c668de90c0dc76dcda82994036653b7947880d0
TzuYin-W <wtzuyin@gmail.com>
2026-08-25 13:12:57 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='finance-tracker-20260825-v32-custom-bg-color-large-cards';
+const CACHE='finance-tracker-20260825-v33-large-card-transparency';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest',
   './icon-192-v23.png','./icon-512-v23.png','./apple-touch-icon-v23.png',
